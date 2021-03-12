d1f7f5fe833fb06fdcc4ac5571cc63bd8c5a2f20
mnstucky <mnstucky@gmail.com>
2021-03-11 22:08:18 -0600

    Update DB connection details for deployment

--- model/db_connect.php
+++ model/db_connect.php
@@ -1,9 +1,10 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username = 'root';
+    $dsn = 'mysql:host=pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=ly1qha9dduvntk37';
+    $username = 'tw73mwrxxzzv6dam';
+    $password = 'qos03u1dnafuy1jm';
     
     try {
-        $db = new PDO($dsn, $username);
+        $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $db_error_msg = $e->getMessage();
         include('../view/error.php');
